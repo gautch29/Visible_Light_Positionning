450c3299ac9b6938f5ee66a98f7f69ca146fb585
Gauthier BARON <gauthier.baron@icloud.com>
2024-07-15 09:44:26 +0700

    Rolling buffer/new sampling frequency/refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,53 @@
 #include "kalmanFilter.h"
 #include "signal.h"
 
-
 void readInputs();
 void printCorrelations();
+void printSignals();
 
 // System parameters ***************************************************************************************************
 double h = 0.35;    // Height of the receiver
 double D = 0.415;    // Distance between reference points
-double be = 60 * PI / 180; // Beam elevation angle
-double al[3] = {0, 120 * PI / 180, -120 * PI / 180}; // Beam azimuth angles
-double p0[3] = {0,0,0};
-double theta0 = 0;
-double ka = 1.7;//5.46; // Placeholder for constant 'ka'
-double m = 2; // Placeholder for constant 'm'
-double P = 1; // Placeholder for constant 'P'
 double p[3][K] = { // Reference point positions
     {0, D, 0, D},
     {0, 0, D, D},
     {h, h, h, h}
 };
 
+double be = 60 * PI / 180; // Beam elevation angle
+double al[3] = {0, 120 * PI / 180, -120 * PI / 180}; // Beam azimuth angles
+
+double p0[3] = {0,0,0}; // Initial position guess
+double theta0 = 0; // Initial angle guess
+
+double ka = 1.7;//5.46; // Placeholder for constant 'ka'
+double m = 2; // Placeholder for constant 'm'
+double P = 1; // Placeholder for constant 'P'
+
+
 int signalFrequencies[K] = {1000, 2000, 4000, 8000};
 
 //Sampling variables ***************************************************************************************************
-double signal[3][SAMPLING_BUFFER_SIZE]; //Buffer for the signal
+int signal[3][SAMPLING_BUFFER_SIZE]; //Buffer for the signal
 IntervalTimer timerSampling; //Timer for sampling
-
-int indexSample = 0;
 ADC *adc = new ADC(); // adc object;
+int indexSample = 0; //Index of the current sample in the rolling buffer
 
 //Correlation variables ************************************************************************************************
-double *maskSignal[K];
+int *maskSignal[K];
 double correlations[K][3];
 unsigned long last_millis_correlation = 0;
-
 
 //Kalman filter ********************************************************************************************************
 KalmanFilter kF = KalmanFilter(p0, theta0, P, al, be, ka, m, h, p);
 unsigned long last_millis_ekf = 0;
-
-//Loop timing **********************************************************************************************************
-unsigned long EKF_period = 20; //Period of the EKF in ms
-unsigned long correlation_period = 20; //Period of the correlation in ms
+unsigned long EKF_period = 50; //Period of the EKF in ms
 
 void setup() {
     Serial.begin(115200);
 
     //Setting up ADC for real fast reading
-    adc->adc0->setAveraging(1);
-    adc->adc0->setResolution(10);
-    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED);
-    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
+    setupADC(adc);
 
     //Generate correlation masks
     for(int i = 0; i < K; i++){
@@ -65,65 +58,51 @@ void setup() {
     }
 
     //Setting up the sampling timer
-    //timerSampling.begin(readInputs, 1000000/SAMPLING_FREQUENCY);
-    //timerSampling.priority(0);
+    timerSampling.begin(readInputs, 1000000/SAMPLING_FREQUENCY);
+    timerSampling.priority(0);
 
 }
 
 void loop() {
-
-    //Correlation
-    if (millis() - last_millis_correlation > correlation_period) {
-        //Update last time
-        last_millis_correlation = millis();
-
-        //Copy signal to avoid interrupr changing it during computation
-        double signalCopy[3][SAMPLING_BUFFER_SIZE];   
-        for(int i = 0; i<3 ; i++){
-            for (int j = 0; j < SAMPLING_BUFFER_SIZE; j++){
-                signalCopy[i][j] = signal[i][j];
-            }
-        }
-        
-        //Compute correlation (i is the signal, j is the sensor)
-        for(int i = 0; i < K; i++){
-            for(int j = 0; j < 3; j++){
-                correlations[i][j] = correlationShift(maskSignal[i], signalCopy[j], CYCLES_IN_MASK * SAMPLING_FREQUENCY/signalFrequencies[i], SAMPLING_FREQUENCY/signalFrequencies[i]);
-            }
-        }
-        //Serial.println(correlations[0][0]);
-        printCorrelations(); //Print correlations
-
-    }
-
-    //EKF
+ 
     if (millis() - last_millis_ekf > EKF_period) {
 
+        timerSampling.end(); //Pause sampling interrupt
+
+        kF.setT((millis() - last_millis_ekf)/1000.0); //Update time step for the EKF
         last_millis_ekf = millis(); //Update last time
 
-        //Update measurements
+        //Correlation ***************************************************************************************************
+        //(i is the signal, j is the sensor)
+        for(int i = 0; i < K; i++){
+            for(int j = 0; j < 3; j++){
+                correlations[i][j] = correlationShift(maskSignal[i], signal[j], CYCLES_IN_MASK * SAMPLING_FREQUENCY/signalFrequencies[i], SAMPLING_FREQUENCY/signalFrequencies[i], indexSample);
+            }
+        }
+        //printCorrelations();   
+        
+        //EKF **********************************************************************************************************
+
         double r[3*K] = {correlations[0][0], correlations[0][1], correlations[0][2], correlations[1][0], correlations[1][1], correlations[1][2], correlations[2][0], correlations[2][1], correlations[2][2], correlations[3][0], correlations[3][1], correlations[3][2]};
         kF.updateMeasurements(r);
+        kF.compute();
+        kF.print();
 
-        kF.compute(); //Compute EKF
-
-        //Forcing y and theta to 0
-        //kF.xp[2] = 0;
-        //kF.xp[4] = 0;
-
-        //kF.print();
+        //End EKF ******************************************************************************************************
+        
+        timerSampling.begin(readInputs, 1000000/SAMPLING_FREQUENCY); //Start sampling again
     }
 }
 
 void readInputs(){
-    //Read the new values
-    signal[0][indexSample] = adc->adc0->analogRead(A0);//analogRead(A0);
-    signal[1][indexSample] = adc->adc0->analogRead(A1);//analogRead(A1);
-    signal[2][indexSample] = adc->adc0->analogRead(A2);//analogRead(A2);
-
     //Shift Index
     indexSample++;
     if(indexSample >= SAMPLING_BUFFER_SIZE - 1) indexSample = 0;
+
+    //Read the new values
+    signal[0][indexSample] = adc->adc0->analogRead(A0);
+    signal[1][indexSample] = adc->adc0->analogRead(A1);
+    signal[2][indexSample] = adc->adc0->analogRead(A2);
 }
 
 void printCorrelations(){
@@ -134,4 +113,14 @@ void printCorrelations(){
         }
     }
     Serial.println();
+}
+
+void printSignals(){
+    for(int i = 0; i < SAMPLING_BUFFER_SIZE; i++){
+        for(int j = 0; j < 3; j++){
+            Serial.print(signal[j][i]);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
 }
